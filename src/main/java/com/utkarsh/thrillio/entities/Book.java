418e8d4b4220dd54e12c86862ca9002fafc3899a
Utkarsh Agarwal <Utkarsh Agarwal@LAPTOP-QFBE83KV>
2021-07-14 21:55:33 +0530

    commit4

--- src/main/java/com/utkarsh/thrillio/entities/Book.java
+++ src/main/java/com/utkarsh/thrillio/entities/Book.java
@@ -1,10 +1,17 @@
 package com.utkarsh.thrillio.entities;
 
-public class Book extends Bookmark {
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.utkarsh.thrillio.constants.BookGenre;
+import com.utkarsh.thrillio.partner.Shareable;
+
+public class Book extends Bookmark implements Shareable{
 	private int publicationYear;
 	private String publisher;
 	private String[] authors;
-	private String genre;
+	private BookGenre genre;
 	private double amazonRating;
 
 	public int getPublicationYear() {
@@ -31,11 +38,11 @@ public class Book extends Bookmark {
 		this.authors = authors;
 	}
 
-	public String getGenre() {
+	public BookGenre getGenre() {
 		return genre;
 	}
 
-	public void setGenre(String genre) {
+	public void setGenre(BookGenre genre) {
 		this.genre = genre;
 	}
 
@@ -45,5 +52,33 @@ public class Book extends Bookmark {
 
 	public void setAmazonRating(double amazonRating) {
 		this.amazonRating = amazonRating;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [publicationYear=" + publicationYear + ", publisher=" + publisher + ", authors="
+				+ Arrays.toString(authors) + ", genre=" + genre + ", amazonRating=" + amazonRating + "]";
+	}
+
+	@Override
+	public boolean isKidFriendlyEligible() {
+		if (genre.equals(BookGenre.PHILOSOPHY) || genre.equals(BookGenre.SELF_HELP)) {
+			return false;
+		}
+		return true;
+	}
+
+	public String getItemData() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("<item");
+		builder.append("<type>Book</type>");
+		builder.append("<title>").append(getTitle()).append("</title>");
+		builder.append("<authors>").append(StringUtils.join(authors,",")).append("</authors>");
+		builder.append("<publisher>").append(publisher).append("</publisher>");
+		builder.append("<publicationYear>").append(publicationYear).append("</publicationYear>");
+		builder.append("<genre>").append(genre).append("</genre>");
+		builder.append("<amazonRating>").append(amazonRating).append("</amazonRating>");
+		builder.append("</item>");
+		return builder.toString();
 	}
 }
